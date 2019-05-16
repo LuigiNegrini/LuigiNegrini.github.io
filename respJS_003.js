1862056e8b6af3f80fc1d4a940d0ab9c88a3fdac
aluno <aluno@lab3q105-p5qc01.ufu.br>
2019-05-16 12:11:21 -0300

    JS_003

---
+++ respJS_003.js
@@ -0,0 +1,21 @@
+var pares = [];
+// Crie aqui suas funções de apoio
+function isPar(numero) {
+    if(numero%2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+function Q1() {
+    console.log('Q1');
+    var aux = document.getElementById('numeroQ1').value;
+    // Segue aqui seu código
+    for(let i = 0; i <= aux; i++) {
+        if(isPar(i)) {
+            pares.push(i);
+        }
+    }
+    console.log(pares);
+    document.getElementById('RQ1').innerHTML = pares;
+}
