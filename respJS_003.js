f1715e9d22374ab954c70879b8bfc97409375476
LuigiNegrini <48721077+LuigiNegrini@users.noreply.github.com>
2019-05-23 09:15:25 -0300

    Add files via upload

--- respJS_003.js
+++ respJS_003.js
@@ -1,6 +1,6 @@
 var pares = [];
 function isPar(numero) {
-    if(numero%2 == 0) {
+    if (numero % 2 == 0) {
         return true;
     } else {
         return false;
@@ -8,17 +8,17 @@ function isPar(numero) {
 }
 function Q1() {
     console.log('Q1');
-    var aux = Number.parseInt(document.getElementById('numeroQ1').value);function isPrimo(numero) {
-    if(numero === 2) { return true; } 
-    else if (numero < 2 || numero % 2 === 0) {	return false; }
-    for(let i = 3; i <= Math.sqrt(numero); i += 2) {
-        if(numero % i === 0) { return false; }
+    var aux = Number.parseInt(document.getElementById('numeroQ1').value); function isPrimo(numero) {
+        if (numero === 2) { return true; }
+        else if (numero < 2 || numero % 2 === 0) { return false; }
+        for (let i = 3; i <= Math.sqrt(numero); i += 2) {
+            if (numero % i === 0) { return false; }
+        }
+        return true;
     }
-    return true;
-}
     // Segue aqui seu código 
-    for(let i = 0; i <= aux; i++) {
-        if(isPar(i)) {
+    for (let i = 0; i <= aux; i++) {
+        if (isPar(i)) {
             pares.push(i);
         }
     }
@@ -33,7 +33,7 @@ function Q1alt() {
     var pares2 = [];
     var aux = document.getElementById('numeroQ1').value;
     // Segue aqui seu código 
-    for(let i = 0; i <= aux; i += 2) {
+    for (let i = 0; i <= aux; i += 2) {
         pares2.push(i);
     }
     console.log(pares2);
@@ -48,37 +48,43 @@ function Q2() {
     var aux = Number.parseInt(document.getElementById('numeroQ2').value);
     // Segue aqui seu código 
     primos.push(2);
-    for(let numero = 3; numero <= aux; numero += 2) {
-            let chk = true;
-	    for(let i = 0; i < primos.length; i++) {
-		if(numero % primos[i] === 0) {
-		    chk = false;
-                    break;
-		}
-	    }
-            if(chk) { primos.push(numero); }
+    for (let numero = 3; numero <= aux; numero += 2) {
+        let chk = true;
+        for (let i = 0; i < primos.length; i++) {
+            if (numero % primos[i] === 0) {
+                chk = false;
+                break;
+            }
+        }
+        if (chk) { primos.push(numero); }
     }
     console.log(primos);
     document.getElementById('RQ2').innerHTML = primos;
 }
 
+
+
 function isPrimo(numero) {
-    if(numero === 2) { return true; } 
-    else if (numero < 2 || numero % 2 === 0) {	return false; }
-    for(let i = 3; i <= Math.sqrt(numero); i += 2) {
-        if(numero % i === 0) { return false; }
+    if (numero === 2) { return true; }
+    else if (numero < 2 || numero % 2 === 0) { return false; }
+    for (let i = 3; i <= Math.sqrt(numero); i += 2) {
+        if (numero % i === 0) { return false; }
     }
     return true;
 }
-
 function Q2alt() {
     var primos2 = [];
     console.log('Q2alt');
     var aux = Number.parseInt(document.getElementById('numeroQ2').value);
     // Segue aqui seu código 
-    for(let i = 0; i <= aux; i++) {
-        if(isPrimo(i)) { primos2.push(i); }
+    for (let i = 0; i <= aux; i++) {
+        if (isPrimo(i)) { primos2.push(i); }
     }
     console.log(primos2);
     document.getElementById('RQ2alt').innerHTML = primos2;
+}
+
+
+function Q3() {
+    
 }
