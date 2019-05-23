5094741092ab093c0341496842014d11bd6f4ac6
LuigiNegrini <48721077+LuigiNegrini@users.noreply.github.com>
2019-05-23 09:06:31 -0300

    Add files via upload

--- respJS_002.js
+++ respJS_002.js
@@ -2,35 +2,53 @@ function Q1() {
     console.log('Q1');
     var x = Number.parseInt(document.getElementById('x').value);
     var y = Number.parseInt(document.getElementById('y').value);
-    // Segue aqui seu código
-    console.log('X / Y: ' + x + ' / ' + y);
-    console.log('Quociente = ' + Math.floor(x/y));
-    console.log('Resto = ' + (x%y));
+    // Segue aqui seu código 
+    var resp = divide(x, y);
+    console.log(resp);
+    document.getElementById('RQ1').innerHTML = resp;
 }
+function divide(x, y) {
+    return 'X / Y: ' + x + ' / ' + y + ' com Quociente = ' + Math.floor(x / y) + '; Resto = ' + (x % y);
+}
+
 
 function Q2() {
     console.log('Q2');
-    var d = Number.parseFloat(document.getElementById('d').value);
-    var e = Number.parseFloat(document.getElementById('e').value);
+    var d = Number.parseInt(document.getElementById('d').value);
+    var e = Number.parseInt(document.getElementById('e').value);
     // Segue aqui seu código
-    console.log('Cateto D:' + d)
-    console.log('Cateto E:' + e)
-    console.log('Hipotenusa:' + Math.sqrt(+ Math.pow(d,2) + + Math.pow(e,2)))
+    var resp = hipotenusa(d, e);
+    console.log(resp);
+    document.getElementById('RQ2').innerHTML = resp;
 }
+function hipotenusa(d, e) {
+    return 'Cateto d :' + d + '; Cateto e :' + e + '; Hipotenusa :' + Math.sqrt(+ Math.pow(d, 2) + + Math.pow(e, 2));
+}
+
 
 function Q3() {
     console.log('Q3');
     var aux = document.getElementById('profissoes');
     var profissao = aux.options[aux.selectedIndex].text;
     // Segue aqui seu código
-    console.log('Profissão:' + profissao)
-    if (profissao == 'Musicista' || profissao == 'Presidente') {console.log('Gênero: Masculino ou Feminino')
-        } else if (profissao.endsWith('a')) {console.log('Gênero: Feminino')
-        } else if (profissao.endsWith('z')) {console.log('Gênero: Feminino')
-        } else if (profissao.endsWith('o')) {console.log('Gênero: Masculino')
-        } else if (profissao.endsWith('r')) {console.log('Gênero: Masculino')
+    var resp = prof(profissao);
+    console.log(resp);
+    document.getElementById('RQ3').innerHTML = resp;
+}
+function prof(profissao) {
+    if (profissao == 'Musicista' || profissao == 'Presidente') {
+        return 'Gênero: Masculino ou Feminino'
+    } else if (profissao.endsWith('a')) {
+        return 'Gênero: Feminino'
+    } else if (profissao.endsWith('z')) {
+        return 'Gênero: Feminino'
+    } else if (profissao.endsWith('o')) {
+        return 'Gênero: Masculino'
+    } else if (profissao.endsWith('r')) {
+        return 'Gênero: Masculino'
     }
 }
+
 
 function Q4() {
     console.log('Q4');
@@ -38,20 +56,20 @@ function Q4() {
     var b_str = document.getElementById('b').value;
     var c_str = document.getElementById('c').value;
     // Segue aqui seu código
-    if (a_str && b_str){
-        console.log('Hipotenusa A =' + a_str)
-        console.log('Cateto B =' + b_str)
-        console.log('Cateto C =' + Math.sqrt(+ Math.pow(a_str,2) - + Math.pow(b_str,2)))
+    var resp = funcao(a_str, b_str, c_str);
+    console.log(resp);
+    document.getElementById('RQ4').innerHTML = resp;
+}
+function funcao(a_str, b_str, c_str) {
+    if (a_str && b_str) {
+        return 'Hipotenusa A =' + a_str + '; Cateto B =' + b_str + '; Cateto C =' + Math.sqrt(+ Math.pow(a_str, 2) - + Math.pow(b_str, 2))
     } else if (a_str && c_str) {
-        console.log('Hipotenusa A =' + a_str);
-        console.log('Cateto B =' + Math.sqrt(+ Math.pow(a_str,2) - + Math.pow(c_str,2)));
-        console.log('Cateto C =' + c_str)
+        return 'Hipotenusa A =' + a_str + '; Cateto B =' + Math.sqrt(+ Math.pow(a_str, 2) - + Math.pow(c_str, 2)) + '; Cateto C =' + c_str
     } else if (b_str && c_str) {
-        console.log('Hipotenusa A =' + Math.sqrt(+ Math.pow(b_str,2) + + Math.pow(c_str,2)))
-        console.log('Cateto B =' + b_str)
-        console.log('Cateto C =' + c_str)
+        return 'Hipotenusa A =' + Math.sqrt(+ Math.pow(b_str, 2) + + Math.pow(c_str, 2)) + '; Cateto B =' + b_str + '; Cateto C =' + c_str
     }
 }
+
 
 function Q5() {
     console.log('Q5');
@@ -60,29 +78,30 @@ function Q5() {
     var aux = document.getElementById('generos');
     var genero = aux.options[aux.selectedIndex].text;
     // Segue aqui seu código
-    var IMC =  + peso / + (+ Math.pow(altura,2))
-    console.log('Gênero :' + genero)
-    console.log('Altura :' + altura)
-    console.log('Peso :' + peso)
-    console.log('IMC :' + IMC)
-    if (genero == 'Feminino'){
-    } else if (IMC < 18.5){
-        console.log('Resultado : Magro')
-    } else if (IMC >= 18.5 && IMC < 25){
-        console.log('Resultado : Sarado')
-    } else if (IMC >= 25 && IMC < 30){
-        console.log('Resultado : Rechonchudo')
-    } else if (IMC > 30){
-        console.log('Resultado : Obeso')
+    var imc =  + peso / + (+ Math.pow(altura, 2));
+    var resp = IMC(genero, peso, altura, imc);
+    console.log(resp);
+    document.getElementById('RQ5').innerHTML = resp;
+}
+function IMC(genero, peso, altura, imc) {
+    if (genero == 'Feminino') {
+    } else if (imc < 18.5) {
+        return 'Gênero :' + genero + '; Altura :' + altura + '; Peso :' + peso + '; Resultado : Magro'
+    } else if (imc >= 18.5 && imc < 25) {
+        return 'Gênero :' + genero + '; Altura :' + altura + '; Peso :' + peso + '; Resultado : Sarado'
+    } else if (imc >= 25 && imc < 30) {
+        return 'Gênero :' + genero + '; Altura :' + altura + '; Peso :' + peso + '; Resultado : Rechonchudo'
+    } else if (imc > 30) {
+        return 'Gênero :' + genero + '; Altura :' + altura + '; Peso :' + peso + '; Resultado : Obeso'
     }
-    if (genero == 'Masculino'){
-    } else if (IMC < 18.5){
-        console.log('Resultado : Magra')
-    } else if (IMC >= 18.5 && IMC < 25){
-        console.log('Resultado : Sarada')
-    } else if (IMC >= 25 && IMC < 30){
-        console.log('Resultado : Rechonchuda')
-    } else if (IMC > 30){
-        console.log('Resultado : Obesa')
+    if (genero == 'Masculino') {
+    } else if (imc < 18.5) {
+        return 'Gênero :' + genero + '; Altura :' + altura + '; Peso :' + peso + '; Resultado : Magra'
+    } else if (imc >= 18.5 && imc < 25) {
+        return 'Gênero :' + genero + '; Altura :' + altura + '; Peso :' + peso + '; Resultado : Sarada'
+    } else if (imc >= 25 && imc < 30) {
+        return 'Gênero :' + genero + '; Altura :' + altura + '; Peso :' + peso + '; Resultado : Rechonchuda'
+    } else if (imc > 30) {
+        return 'Gênero :' + genero + '; Altura :' + altura + '; Peso :' + peso + '; Resultado : Obesa'
     }
 }
--- respJS_003.js
+++ respJS_003.js
@@ -1,7 +1,6 @@
 var pares = [];
-// Crie aqui suas funções de apoio
 function isPar(numero) {
-    if(numero%2 === 0){
+    if(numero%2 == 0) {
         return true;
     } else {
         return false;
@@ -9,8 +8,15 @@ function isPar(numero) {
 }
 function Q1() {
     console.log('Q1');
-    var aux = document.getElementById('numeroQ1').value;
-    // Segue aqui seu código
+    var aux = Number.parseInt(document.getElementById('numeroQ1').value);function isPrimo(numero) {
+    if(numero === 2) { return true; } 
+    else if (numero < 2 || numero % 2 === 0) {	return false; }
+    for(let i = 3; i <= Math.sqrt(numero); i += 2) {
+        if(numero % i === 0) { return false; }
+    }
+    return true;
+}
+    // Segue aqui seu código 
     for(let i = 0; i <= aux; i++) {
         if(isPar(i)) {
             pares.push(i);
@@ -20,4 +26,59 @@ function Q1() {
     document.getElementById('RQ1').innerHTML = pares;
 }
 
-var
+
+
+function Q1alt() {
+    console.log('Q1');
+    var pares2 = [];
+    var aux = document.getElementById('numeroQ1').value;
+    // Segue aqui seu código 
+    for(let i = 0; i <= aux; i += 2) {
+        pares2.push(i);
+    }
+    console.log(pares2);
+    document.getElementById('RQ1alt').innerHTML = pares2;
+}
+
+
+
+var primos = [];
+function Q2() {
+    console.log('Q2');
+    var aux = Number.parseInt(document.getElementById('numeroQ2').value);
+    // Segue aqui seu código 
+    primos.push(2);
+    for(let numero = 3; numero <= aux; numero += 2) {
+            let chk = true;
+	    for(let i = 0; i < primos.length; i++) {
+		if(numero % primos[i] === 0) {
+		    chk = false;
+                    break;
+		}
+	    }
+            if(chk) { primos.push(numero); }
+    }
+    console.log(primos);
+    document.getElementById('RQ2').innerHTML = primos;
+}
+
+function isPrimo(numero) {
+    if(numero === 2) { return true; } 
+    else if (numero < 2 || numero % 2 === 0) {	return false; }
+    for(let i = 3; i <= Math.sqrt(numero); i += 2) {
+        if(numero % i === 0) { return false; }
+    }
+    return true;
+}
+
+function Q2alt() {
+    var primos2 = [];
+    console.log('Q2alt');
+    var aux = Number.parseInt(document.getElementById('numeroQ2').value);
+    // Segue aqui seu código 
+    for(let i = 0; i <= aux; i++) {
+        if(isPrimo(i)) { primos2.push(i); }
+    }
+    console.log(primos2);
+    document.getElementById('RQ2alt').innerHTML = primos2;
+}
